416874c41126e916f9546ab7d4908054c53b274a
Willian Ferreira <williannfferreira@gmail.com>
2023-06-16 14:10:34 -0300

    fix: error route

--- src/services/room-service.ts
+++ src/services/room-service.ts
@@ -1,8 +1,10 @@
-import axios from 'axios';
-
+// import axios from 'axios';
 export const RoomService = {
   async turnOn(id: number) {
-    return axios.get(
+    // const agent = new https.Agent({
+    //   rejectUnauthorized: false,
+    // });
+    return fetch(
       `https://793434.homemanager.com.br:9090/HomeWeb/command.do?valor=X&id=${id}`
     );
   },
