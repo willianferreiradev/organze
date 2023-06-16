79eff81b7fe4fc1b238865b4aa3ab48ff87ed8fc
Willian Ferreira <williannfferreira@gmail.com>
2023-06-16 14:30:46 -0300

    fix: error route

--- src/services/room-service.ts
+++ src/services/room-service.ts
@@ -3,13 +3,13 @@ import axios from 'axios';
 export const RoomService = {
   async turnOn(id: number) {
     return axios.get(
-      `https://793434.homemanager.com.br:9090/HomeWeb/command.do?valor=X&id=${id}`
+      `http://793434.homemanager.com.br:9090/HomeWeb/command.do?valor=X&id=${id}`
     );
   },
 
   async turnOnAll() {
     return axios.get(
-      `https://793434.homemanager.com.br:9090/HomeWeb/command.do?valor=X&id=104`
+      `http://793434.homemanager.com.br:9090/HomeWeb/command.do?valor=X&id=104`
     );
   },
 };
