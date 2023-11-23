a7c4c12ff4d931d81635f0469799a13e32f809ef
Willian Ferreira <williannfferreira@gmail.com>
2023-11-23 16:39:06 -0300

    adiciona imagem

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,9 @@
 import * as S from './styles';
 
-import organze from '../../../public/organze.png';
-
 export function Header() {
   return (
     <S.Container>
-      <img src={organze} alt="Logo da Organze" />
+      <img src="/organze.png" alt="Logo da Organze" />
     </S.Container>
   );
 }
