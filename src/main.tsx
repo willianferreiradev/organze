7a43c678dcb280ed18fef79443026c40aea408f0
Willian Ferreira <williannfferreira@gmail.com>
2023-11-24 10:34:54 -0300

    teste sem importação

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+// import App from './App.tsx';
 import './index.css';
-import { Home } from './pages/Home/index.tsx';
-import { Room } from './pages/Room/index.tsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// import { Home } from './pages/Home/index.tsx';
+// import { Room } from './pages/Room/index.tsx';
+// import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: 'room/:id',
-        element: <Room />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <Home />,
+//       },
+//       {
+//         path: 'room/:id',
+//         element: <Room />,
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div>
+      <h1 style={{ color: 'white' }}>teste</h1>
+    </div>
+    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>
 );
